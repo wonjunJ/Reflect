292cc25a4cfbd479a47becd65e8e2ece6e964674
이상수 <newtkdtn@gmail.com>
2024-01-20 11:46:18 +0900

    20240120

---
+++ MadeMe/backend/mademe/src/main/java/com/ssafy/mademe/MademeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mademe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MademeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MademeApplication.class, args);
+	}
+
+}
---
+++ MadeMe/backend/mademe/src/test/java/com/ssafy/mademe/MademeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.mademe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MademeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
